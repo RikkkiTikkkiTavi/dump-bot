220083d2d79d7a4155b06f54cb9e2669bfb769d7
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2024-08-11 11:02:39 +0300

    ref:rabbitListener param

--- node/src/main/java/org/example/service/ConsumerServiceImpl.java
+++ node/src/main/java/org/example/service/ConsumerServiceImpl.java
@@ -1,39 +1,37 @@
 package org.example.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import static org.example.model.RabbitQueue.*;
 
-@Service
 @Log4j
+@RequiredArgsConstructor
+@Service
 public class ConsumerServiceImpl implements ConsumerService {
+
     private final MainService mainService;
 
-    public ConsumerServiceImpl(MainService mainService) {
-        this.mainService = mainService;
+    @Override
+    @RabbitListener(queues = "${spring.rabbitmq.queues.text-message-update}")
+    public void consumeTextMessageUpdate(Update update) {
+        log.debug("NODE: Text message is received");
+        mainService.processTextMessage(update);
     }
 
     @Override
-    @RabbitListener(queues = PHOTO_MESSAGE_UPDATE)
-    public void consumePhotoMessageUpdate(Update update) {
-        log.debug("NODE: Photo message is received");
-        mainService.processPhotoMessage(update);
-    }
-
-    @Override
-    @RabbitListener(queues = DOC_MESSAGE_UPDATE)
+    @RabbitListener(queues = "${spring.rabbitmq.queues.doc-message-update}")
     public void consumeDocMessageUpdate(Update update) {
         log.debug("NODE: Doc message is received");
         mainService.processDocMessage(update);
     }
 
     @Override
-    @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
-    public void consumeTextMessageUpdate(Update update) {
-        log.debug("NODE: Text message is received");
-        mainService.processTextMessage(update);
+    @RabbitListener(queues = "${spring.rabbitmq.queues.photo-message-update}")
+    public void consumePhotoMessageUpdate(Update update) {
+        log.debug("NODE: Photo message is received");
+        mainService.processPhotoMessage(update);
     }
 }
