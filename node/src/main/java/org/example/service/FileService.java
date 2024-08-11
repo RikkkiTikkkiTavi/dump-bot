6e868dc74eedb59961bbfa43a6c301900e293db9
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2024-08-11 22:26:01 +0300

    ref: add method for generate link

--- node/src/main/java/org/example/service/FileService.java
+++ node/src/main/java/org/example/service/FileService.java
@@ -2,9 +2,11 @@ package org.example.service;
 
 import org.example.entity.AppDocument;
 import org.example.entity.AppPhoto;
+import org.example.service.enums.LinkType;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
 public interface FileService {
     AppDocument processDoc(Message externalMessage);
     AppPhoto processPhoto(Message externalMessage);
+    String generateLink(Long docId, LinkType linkType);
 }
