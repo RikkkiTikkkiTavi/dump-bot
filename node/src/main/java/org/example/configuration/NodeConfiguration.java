d5a12ff0273bb49efd9dce8e449ecde5bc029031
Constantin <Rikkki.Tikkki.Tavi@yandex.ru>
2024-08-12 13:14:24 +0300

    feat:add salt

---
+++ node/src/main/java/org/example/configuration/NodeConfiguration.java
@@ -0,0 +1,17 @@
+package org.example.configuration;
+
+import org.example.utils.CryptoTool;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NodeConfiguration {
+    @Value("${salt}")
+    private String salt;
+
+    @Bean
+    public CryptoTool getCryptoTool() {
+        return new CryptoTool(salt);
+    }
+}
